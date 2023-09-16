67d2133f127390bd61dd981fc2961134b4e191ea
tverdoy <tverdoy.work@gmail.com>
2023-09-16 20:13:51 +0300

    Remove throws for getUrl in IRequest

--- src/main/java/tverdoy/healthcheck/IRequest.java
+++ src/main/java/tverdoy/healthcheck/IRequest.java
@@ -1,7 +1,5 @@
 package tverdoy.healthcheck;
 
-import java.util.NoSuchElementException;
-
 public interface IRequest {
     /**
      * @return an address that must be checked
@@ -12,5 +10,5 @@ public interface IRequest {
     /**
      * @return an url that must be checked
      */
-    String getUrl() throws NoSuchElementException;
+    String getUrl();
 }
